77005175e9ab4896212bd607c2b8cd9aec2435a2
Dan77298 <84914606+Dan77298@users.noreply.github.com>
2023-05-22 03:46:41 +1000

    Tile properties and Material display
    Tiles have properties associated with their type
    methods have been added to switch between material states (onFire, onEmber, extinguished)
    Tiles render their material when their state changes

---
+++ Firebreak tactics/Assets/Code/Scripts/MaterialChanger.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer objectRenderer;
+    [SerializeField]
+    private TileType defaultTile;
+    private TileType current;
+
+    public enum TileType
+    {
+        Grass1,
+        Grass2,
+        Grass3,
+        Dirt,
+        Fire,
+        Water,
+        Ember
+    }
+
+    private void Awake()
+    {
+        current = defaultTile; // Set the initial material option
+        SetTile(current);
+    }
+
+    private void SetMaterial()
+    {
+        switch (current)
+        {
+            case TileType.Dirt:
+                objectRenderer.material = GetMaterialFromName("Dirt");
+                break;
+            case TileType.Fire:
+                objectRenderer.material = GetMaterialFromName("Fire");
+                break;
+            case TileType.Grass1:
+                objectRenderer.material = GetMaterialFromName("Grass1");
+                break;
+            case TileType.Grass2:
+                objectRenderer.material = GetMaterialFromName("Grass2");
+                break;   
+            case TileType.Grass3:
+                objectRenderer.material = GetMaterialFromName("Grass3");
+                break;  
+            case TileType.Water:
+                objectRenderer.material = GetMaterialFromName("Water");
+                break; 
+            case TileType.Ember:
+                objectRenderer.material = GetMaterialFromName("Ember");
+                break; 
+            // Add cases for other materials here
+
+            default:
+                break;
+        }
+    }
+
+    private Material GetMaterialFromName(string materialName)
+    {
+        // Assuming the materials are stored in a "Materials" folder in the Resources folder
+        string path = "Materials/" + materialName;
+        return Resources.Load<Material>(path);
+    }
+
+    public void SetTile(TileType tile)
+    {
+        current = tile;
+        SetMaterial();
+    }
+
+    public void SetDefault()
+    {
+        current = defaultTile;
+        SetMaterial();
+    }
+}
---
+++ Firebreak tactics/Assets/Code/Scripts/TileBehaviour.cs
@@ -0,0 +1,198 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour
+{
+    // code inheritance
+    [SerializeField]
+    private MaterialChanger materialChanger; // handles changing materials 
+
+    // tile states
+    [SerializeField]
+    private TileType tile; // what type of tile is it
+    private TileType defaultTile; // what type of tile was it originally 
+    private bool onFire = false; // is the tile burning
+    private bool onEmber = false; // is the tile embering 
+    private bool hasEmbered = false; // has the tile used its ember state 
+
+    private Material tileMaterial; // the tile's default material 
+    private Material displayMaterial; // the tile's displayed material 
+
+    // tile traversal
+    private int altitude = 1; // [1]low, [2]medium, [3]high 
+    private int traversalCost; // cost of traversing the tile 
+    private int traversalRule; // [1]all units, [2]ground only, [3]air only, [4] no traversal
+
+    // tile health
+    private int vegetation; // how much health a tile has (max health)  
+    private int decay; // how much health a tile has left (remaining)
+	
+
+    public enum TileType{
+        Grass1, // veg 1, 
+        Grass2,
+        Grass3,
+        Dirt,
+        Fire,
+        Water,
+        Ember
+    }
+
+    private void Awake(){
+        ChangeTileState(tile); 
+        SetDefaultState(tile);
+    }
+
+    private void SetDefaultState(TileType _tile){
+        switch (_tile){
+            case TileType.Grass1:
+                vegetation = 1;
+                traversalCost = 1;
+                traversalRule = 1;
+                displayMaterial = tileMaterial;
+                onFire = false;
+                hasEmbered = false;
+                decay = 4;
+                break;
+            case TileType.Grass2:
+                vegetation = 2;
+                traversalCost = 1;
+                traversalRule = 1;
+                onFire = false;
+                hasEmbered = false;
+                decay = 5;
+                break;
+            case TileType.Grass3:
+                vegetation = 3;
+                traversalCost = 1;
+                traversalRule = 1;
+                onFire = false;
+                hasEmbered = false;
+                decay = 6;
+                break;
+            case TileType.Dirt:
+                vegetation = 0;
+                hasEmbered = true;
+                traversalCost = 1;
+                traversalRule = 1;
+                onFire = false;
+                hasEmbered = false;
+                break;
+            case TileType.Water:
+                vegetation = 0;
+                hasEmbered = true;
+                traversalCost = 1;
+                traversalRule = 3;
+                onFire = false;
+                hasEmbered = true;
+                break;
+            case TileType.Fire:
+                onFire = true;
+                vegetation = 0;
+                decay = 6;
+                traversalCost = 2;
+                traversalRule = 3;
+                hasEmbered = false;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ChangeTileState(TileType _tile){
+        // changes made to tile variables exclusively for switching states to/from ember/fire
+        switch (_tile){
+            case TileType.Grass1:
+                onFire = false;
+                break;
+            case TileType.Grass2:
+                onFire = false;
+                break;
+            case TileType.Grass3:
+                onFire = false;
+                break;
+            case TileType.Dirt:
+                vegetation = 0;
+                hasEmbered = true;
+                onFire = false;
+                hasEmbered = true;
+                break;
+            case TileType.Fire:
+                onFire = true;
+                break;
+            case TileType.Ember:
+                onEmber = true;
+                hasEmbered = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public bool GetOnFire(){
+        return this.onFire;
+    }
+
+    public bool GetOnEmber(){
+        return this.onEmber;
+    }
+
+    public void SetOnFire(){
+        if (CanOnFire()){
+            // can tile be ignited
+            ChangeTileState(TileType.Fire);
+            changeMaterial();
+        }
+    }
+
+    public void SetOnEmber(){
+        if (CanOnEmber()){
+            // can tile be embered
+            ChangeTileState(TileType.Ember);
+            changeMaterial();
+        }
+    }
+
+    public void Extinguish(){
+        // extinguish a burning tile
+        if (GetOnFire() || GetOnEmber()){
+            // if the tile is on fire or embering
+            ChangeTileState(GetDefaultTile());
+            changeMaterial();
+        }
+    }
+
+    public TileType GetDefaultTile(){
+        // returns the defaultTile
+        return this.defaultTile;
+    }
+
+    public TileType GetTileType(){
+        // returns the currentTile
+        return this.tile;
+    }
+
+
+    public bool CanOnFire(){
+        if (!onFire && decay > 0 && vegetation > 0){
+        // if the tile isn't on fire, had fuel to begin with, and has fuel remaining 
+            return true;
+        }
+        return false;
+    }
+
+    public bool CanOnEmber()
+    {
+        if (!onEmber && !onFire && hasEmbered && decay > 0 && vegetation > 0){
+        // if the tile isn't on fire, had fuel to begin with, hasn't been embered already, and has fuel remaining
+            return true;
+        }
+        return false;
+    }
+
+    private void changeMaterial(){
+        MaterialChanger.TileType materialType = (MaterialChanger.TileType)tile;
+        materialChanger.SetTile(materialType);
+    }
+}
