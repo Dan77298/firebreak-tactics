45ca36bb08e83fe04647cbfc38dfa63da0927df5
Lachlan Smyth <133283532+c3206059@users.noreply.github.com>
2023-09-12 15:35:04 +1000

    Health Bar progress

--- Firebreak tactics/Assets/Code/Scripts/UI/HealthBar.cs
+++ Firebreak tactics/Assets/Code/Scripts/UI/HealthBar.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    private int totalTiles = 676;
-    private int burntTiles;
-    private int fireTiles;
-    private int forest1Tiles;
-    private int forest2Tiles;
-    private int forest3Tiles;
-    private int waterTiles;
+    [SerializeField] private TileManager tileManager;
+    private int totalTiles = 676; // Replace with actual number of tiles
+    private int burntTiles = 0; // Replace with global variables?
+    private int fireTiles = 40;
+    private int forest1Tiles = 200;
+    private int forest2Tiles = 150;
+    private int forest3Tiles = 100;
+    private int waterTiles = 30;
+    [SerializeField] public Image burntBar;
     [SerializeField] public Image fireBar;
+    [SerializeField] public Image forest1Bar;
+    [SerializeField] public Image forest2Bar;
+    [SerializeField] public Image forest3Bar;
+    [SerializeField] public Image waterBar;
     
     private void Awake()
     {
@@ -25,17 +31,28 @@ public class HealthBar : MonoBehaviour
     }
     
     private float calculateBarSize(int tiles) {
-        // For testing
-        tiles = UnityEngine.Random.Range(0, 600);
-        Debug.Log(tiles);
-        float percentage = (float)tiles/totalTiles;
-        Debug.Log(percentage * 500);
-        return (float)percentage * 500;
+        float percentage = (float) tiles/totalTiles;
+        return (float) percentage * 500;
     }
 
     private void GameStateChanged(GameManager.GameState _state){
-        float fireBarSize = calculateBarSize(fireTiles);
+        float burntBarSize = calculateBarSize(burntTiles);
+        changeImageSize(burntBar, new Vector2(burntBarSize, 30f));
+        
+        float fireBarSize = calculateBarSize(tileManager.GetFireTiles().Count);
         changeImageSize(fireBar, new Vector2(fireBarSize, 30f));
+        
+        float forest1BarSize = calculateBarSize(forest1Tiles);
+        changeImageSize(forest1Bar, new Vector2(forest1BarSize, 30f));
+        
+        float forest2BarSize = calculateBarSize(forest2Tiles);
+        changeImageSize(forest2Bar, new Vector2(forest2BarSize, 30f));
+        
+        float forest3BarSize = calculateBarSize(forest3Tiles);
+        changeImageSize(forest3Bar, new Vector2(forest3BarSize, 30f));
+        
+        float waterBarSize = calculateBarSize(waterTiles);
+        changeImageSize(waterBar, new Vector2(waterBarSize, 30f));
     }
     
     private void changeImageSize(Image image, Vector2 size)
